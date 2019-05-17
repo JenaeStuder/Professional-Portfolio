964f2fe40dbf3816aab2b589b1ac17114dfa40b9
Jenae <Jenaenay15@yahoo.com>
2019-05-17 15:57:33 -0700

    added a path

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3001;
+const path = require ('path');
 
 if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
   }
   // Add routes, both API and view
   app.use(routes);
+
+  app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
   app.listen(PORT, function() {
     console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
