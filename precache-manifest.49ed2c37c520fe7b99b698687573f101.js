6070ef5fc28aa012aa4147ff8f714ee015de8ce0
Jenae <Jenaenay15@yahoo.com>
2019-05-17 15:20:19 -0700

    Updates

---
+++ precache-manifest.49ed2c37c520fe7b99b698687573f101.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5a11f147ee4b27365e10",
+    "url": "/static/css/main.cb292930.chunk.css"
+  },
+  {
+    "revision": "5a11f147ee4b27365e10",
+    "url": "/static/js/main.2346484a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "396bdfff239849ed1165",
+    "url": "/static/js/2.792ce597.chunk.js"
+  },
+  {
+    "revision": "6a196d8d865211d89844e905733d42d4",
+    "url": "/static/media/n2tsadvuTKGxEwyC+IA5DQ_thumb_12dcf.6a196d8d.jpg"
+  },
+  {
+    "revision": "0f62d93a28d88e785aeee879b0dd22bf",
+    "url": "/static/media/florals1.0f62d93a.png"
+  },
+  {
+    "revision": "1d4cb7a907c0770b3bf6e610dfaaedd0",
+    "url": "/index.html"
+  }
+];
