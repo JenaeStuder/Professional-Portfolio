40ece911ea54d6defae5ab749816af5acc66a1a1
Jenae <Jenaenay15@yahoo.com>
2019-06-21 09:48:14 -0700

    added melt internship test to the portfolio and added project descriptions

--- client/src/pages/Projects.js
+++ client/src/pages/Projects.js
@@ -60,9 +60,14 @@ class Projects extends Component {
                         </Col>
                     </Row>
                     <Row>
-                        <Col xs={12} md={3} id="Columns" >
+                        <Col xs={12} md={4} id="Columns" >
                             <a className='Links' id='Portfolios' href='https://jenaestuder.com'>My Portfolio</a>
                             <br />
+                            <Row Classname='description'>
+                                <Col xs={12}>
+                                    <p className='Lists'>My personal portfolio that you are currently viewing. It was built for fun to showcase my abilites as a full stack developer.</p>
+                                </Col> 
+                            </Row>
                             <Row className="space">
                                 <Col xs={1}></Col>
                                 <Col xs={12}>
@@ -79,16 +84,20 @@ class Projects extends Component {
                                         <li>React-Bootstrap</li>
                                         <li>JavaScript</li>
                                         <li>CSS</li>
-                                        <li>PHP</li>
                                         <li>HTML</li>
                                     </ul>
                                 </Col>
                                 <Col xs={1}></Col>
                             </Row>
                         </Col>
-                        <Col xs={12} md={3}id="Columns" >
+                        <Col xs={12} md={4} id="Columns" >
                             <a className="Links" id='Vans' href="https://ancient-escarpment-24807.herokuapp.com/">Van's Memory Game</a>
                             <br></br>
+                            <Row Classname='description'>
+                                <Col xs={12}>
+                                    <p className='Lists'>An interactive memory game created to show elevation of state in ReactJS. Inspiration came from my favorite shoe brand.</p>
+                                </Col> 
+                            </Row>
                             <Row className="space">
                                 <Col xs={1}></Col>
                                 <Col xs={12}>
@@ -109,9 +118,14 @@ class Projects extends Component {
                                 <Col xs={1}></Col>
                             </Row>
                         </Col>
-                        <Col xs={12} md={3} id="Columns">
+                        <Col xs={12} md={4} id="Columns">
                             <a className="Links" id='OffThe' href='https://off-the-beaten-path.herokuapp.com/' >Off the Beaten Path</a>
                             <br></br>
+                            <Row Classname='description'>
+                                <Col xs={12}>
+                                    <p className='Lists'>A full stack application built with the Create and Update functions of CRUD. It's an application that helps you find your perfect obscure vacation destination.</p>
+                                </Col> 
+                            </Row>
                             <Row className="space">
                                 {/* <Col xs={1}></Col> */}
                                 <Col xs={12}>
@@ -137,9 +151,17 @@ class Projects extends Component {
                                 </Col>
                             </Row>
                         </Col>
-                        <Col xs={12} md={3} id="Columns">
+                    </Row>
+                    <Row>
+                        <Col md={2}></Col>
+                        <Col xs={12} md={4} id="Columns">
                             <a className="Links" id='GOT' href='https://groodymoloko.github.io/gameofthrones/'> Game of Thrones Portal</a>
                             <br></br>
+                            <Row Classname='description'>
+                                <Col xs={12}>
+                                    <p className='Lists'>An entirely front end application that calls on multiple GOT API's to reference information on your favorite GOT characters and houses.</p>
+                                </Col> 
+                            </Row>
                             <Row className="space">
                                 <Col xs={12}>
                                     <p className='Heading'>Created Using:</p>
@@ -160,30 +182,60 @@ class Projects extends Component {
                                 </Col>
                             </Row>
                         </Col>
-                    </Row>
-                    <Row className="End">
-                        <Col xs={1}></Col>
-                        <Col xs={10}>
-                            <h3 className='Text'>For a complete and always updated list please check out my
-                        <a className='Texts' href='https://github.com/naeNae15232?tab=repositories' alt='github link'> Github.</a></h3>
+                        <Col xs={12} md={4} id="Columns">
+                            <a className="Links" id='intern' href='https://naenae15232.github.io/melt-internship/'> meltmedia Test</a>
+                            <br></br>
+                            <Row Classname='description'>
+                                <Col xs={12}>
+                                    <p className='Lists'>A fully responsive website built to showcase my abilities as a Front End Dev for meltmedia. I had to recreate a PDF and add my own creativity where applicable.</p>
+                                </Col> 
+                            </Row>
+                            <Row className="space">
+                                <Col xs={12}>
+                                    <p className='Heading'>Created Using:</p>
+                                </Col>
+                            </Row>
+                            <Row>
+                                <Col xs={10}>
+                                    <ul className="Lists">
+                                        <li>JavaScript</li>
+                                        <li>jQuery</li>
+                                        <li>iFrames</li>
+                                        <li>HTML</li>
+                                        <li>CSS</li>
+                                        <li>Bootstrap</li>
+                                    </ul>
+                                </Col>
+                            </Row>
                         </Col>
-                        <Col xs={1}></Col>
+                        <Col md={2}></Col>
                     </Row>
-                </Container>
-                <Footer
-                    copyrights="copy 2019 Jenae Studer-Hart"
-                    moreLinks={<a />}
-                    links={<ul />}
-                    className="Footer"
-                >
-                    {/* <h5 className="white-text">
+                        
+            <Row className="End">
+                <Col xs={1}></Col>
+                <Col xs={10}>
+                    <h3 className='Text'>For a complete and always updated list please check out my
+                        <a className='Texts' href='https://github.com/naeNae15232?tab=repositories' alt='github link'> Github.</a></h3>
+                </Col>
+                <Col xs={1}></Col>
+            </Row>
+            <br />
+                </Container >
+
+            <Footer
+                copyrights="copy 2019 Jenae Studer-Hart"
+                moreLinks={<a />}
+                links={<ul />}
+                className="Footer"
+            >
+                {/* <h5 className="white-text">
                     Created in Reactjs
 </h5> */}
-                    <p className="grey-text text-lighten-4">
+                <p className="grey-text text-lighten-4">
 
-                    </p>
-                </Footer>
-            </div>
+                </p>
+            </Footer>
+            </div >
         )
     }
 };
